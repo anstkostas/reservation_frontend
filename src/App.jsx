50b7c461336c36177e00fdbc06ce4da7707cf50e
anstkostas <kanastasiou.dev@gmail.com>
2026-01-11 23:31:45 +0200

    Feat: Add sonner shadcn/ui component to implement Toaster(success/error) toasts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
-import { Providers } from './app/providers'  
+import { Providers } from './app/providers'
 import { Router } from './app/Router'
+import { Toaster } from "@/components/ui/sonner"
 
 function App() {
   return (
     <BrowserRouter>
       <Providers>
         <Router />
+        <Toaster />
       </Providers>
     </BrowserRouter>
   )
